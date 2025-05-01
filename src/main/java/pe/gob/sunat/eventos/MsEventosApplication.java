4d4ecb3e1dec04fae93041a6c9e6cfb4bca37652
Jean <jgarciaal@192.168.1.87>
2025-04-30 21:23:00 -0500

    init

---
+++ src/main/java/pe/gob/sunat/eventos/MsEventosApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.sunat.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/gob/sunat/eventos/MsEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.sunat.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
